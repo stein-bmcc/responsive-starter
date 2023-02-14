74ecb46bdb365a0586b22b06d30bab483cba579d
profstein <profstein@users.noreply.github.com>
2023-02-14 19:45:31 +0000

    add main.js with IIFE for jquery

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// IIFE - Immediately Invoked Function Expression
+// this is used instead of $("document").ready(function()
+//that you may see in examples or places you copy code
+(function ($, window, document) {
+
+  // Write your jQuery JavaScript here!
+  
+})(window.jQuery, window, document);
+// The global jQuery object is passed as a parameter
